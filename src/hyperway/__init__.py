a75e6d78f74100b7a2d5f05057b759bea956d7af
Jay Jagpal <jay@strangemother.com>
2024-07-08 16:57:05 +0100

    Better imports

--- src/hyperway/__init__.py
+++ src/hyperway/__init__.py
@@ -2,5 +2,6 @@ from . import edges
 from . import nodes
 from . import stepper
 
+from .nodes import as_unit, as_units, Unit
 from .graph import Graph
 from .packer import argspack, argpack, ArgsPack
